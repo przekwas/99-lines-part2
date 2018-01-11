e8bf94a2b9914d6d66efc22dc02447c43b7b893f
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-11 11:21:13 -0600

    nest loops, dom each div with h3 and p

--- scripts.js
+++ scripts.js
@@ -19,33 +19,74 @@ let friends = ["Audrey", "Yzerman", "Apollo", "Chris", "Josh"];
 //outer for loop to generate a song for each friend
 function songOuter(whichFriend) {
     for (let j = 0; j < whichFriend.length; j++) {
-        console.log(whichFriend[j].toUpperCase() + ":");
-        songInner(whichFriend[j]);
-    }
-}
 
-//inner for loop to generate 100 lines of lyrics per friend
-function songInner(currentFriend) {
-    for (let i = 100; i > 0; i--) {
-        let linesLeft = i;
-        let paragraph = document.createElement("p");
-        //catch for final line of the song to correct the grammar
-        if (i === 1) {
-            let paragraphText = document.createTextNode(linesLeft + " line of code in the file, " + linesLeft + " line of code; " + currentFriend + " strikes one out, clears it all out, no more lines of code in the file");
-            paragraph.appendChild(paragraphText);
-            document.body.appendChild(paragraph);
-        //catch for penultimate line of the song to correct the grammar
-        } else if (i === 2) {
-            let oneLessLine = linesLeft - 1;
-            let paragraphText = document.createTextNode(linesLeft + " lines of code in the file, " + linesLeft + " lines of code; " + currentFriend + " strikes one out, clears it all out, " + oneLessLine + " line of code in the file");
-            paragraph.appendChild(paragraphText);
-            document.body.appendChild(paragraph);
-       //standard line for the song 
-        } else {
-            let oneLessLine = linesLeft - 1;
-            let paragraphText = document.createTextNode(linesLeft + " lines of code in the file, " + linesLeft + " lines of code; " + currentFriend + " strikes one out, clears it all out, " + oneLessLine + " lines of code in the file");
-            paragraph.appendChild(paragraphText);
-            document.body.appendChild(paragraph);
+        //creates a div for each iteration with class name friend
+        let friendDiv = document.createElement("div");
+        friendDiv.className = "friend";
+
+        //creates a h3 for friends name and add it to the div
+        let friendHeading = document.createElement("h3");
+        friendDiv.appendChild(friendHeading);
+
+        //adds the div w/ class friend to body
+        document.body.appendChild(friendDiv);
+        //adds friends name as h3 in div w/ class friend
+        friendHeading.appendChild(document.createTextNode(whichFriend[j].toUpperCase() + ": "));
+
+        //inner loop to generate each line
+        for (let i = 100; i > 0; i--) {
+            let linesLeft = i;
+
+            //catch for final line of the song to correct the grammar
+            if (i === 1) {
+                let paragraph = document.createElement("p");
+                let paragraphText = document.createTextNode(linesLeft + " line of code in the file, " + linesLeft + " line of code; " + whichFriend[j] + " strikes one out, clears it all out, no more lines of code in the file");
+                paragraph.appendChild(paragraphText);
+                friendDiv.appendChild(paragraph);
+
+            //catch for penultimate line of the song to correct the grammar
+            } else if (i === 2) {
+                let paragraph = document.createElement("p");
+
+                let oneLessLine = linesLeft - 1;
+                let paragraphText = document.createTextNode(linesLeft + " lines of code in the file, " + linesLeft + " lines of code; " + whichFriend[j] + " strikes one out, clears it all out, " + oneLessLine + " line of code in the file");
+                paragraph.appendChild(paragraphText);
+                friendDiv.appendChild(paragraph);
+
+            //standard line for the song 
+            } else {
+                let paragraph = document.createElement("p");
+                let oneLessLine = linesLeft - 1;
+                let paragraphText = document.createTextNode(linesLeft + " lines of code in the file, " + linesLeft + " lines of code; " + whichFriend[j] + " strikes one out, clears it all out, " + oneLessLine + " lines of code in the file");
+                paragraph.appendChild(paragraphText);
+                friendDiv.appendChild(paragraph);
+            }
         }
     }
 }
+
+// //inner for loop to generate 100 lines of lyrics per friend
+// function songInner(currentFriend) {
+//     for (let i = 100; i > 0; i--) {
+//         let linesLeft = i;
+//         let paragraph = document.createElement("p");
+//         //catch for final line of the song to correct the grammar
+//         if (i === 1) {
+//             let paragraphText = document.createTextNode(linesLeft + " line of code in the file, " + linesLeft + " line of code; " + currentFriend + " strikes one out, clears it all out, no more lines of code in the file");
+//             paragraph.appendChild(paragraphText);
+//             document.body.appendChild(paragraph);
+//             //catch for penultimate line of the song to correct the grammar
+//         } else if (i === 2) {
+//             let oneLessLine = linesLeft - 1;
+//             let paragraphText = document.createTextNode(linesLeft + " lines of code in the file, " + linesLeft + " lines of code; " + currentFriend + " strikes one out, clears it all out, " + oneLessLine + " line of code in the file");
+//             paragraph.appendChild(paragraphText);
+//             document.body.appendChild(paragraph);
+//             //standard line for the song 
+//         } else {
+//             let oneLessLine = linesLeft - 1;
+//             let paragraphText = document.createTextNode(linesLeft + " lines of code in the file, " + linesLeft + " lines of code; " + currentFriend + " strikes one out, clears it all out, " + oneLessLine + " lines of code in the file");
+//             paragraph.appendChild(paragraphText);
+//             document.body.appendChild(paragraph);
+//         }
+//     }
+// }
